36a3852862cea9dbc76b1d09016ee4f1d597b3a0
Ben Linzel <9991873+benlinzel@users.noreply.github.com>
2021-03-22 18:46:21 -0400

    move method

--- temp_sensor.py
+++ temp_sensor.py
@@ -37,17 +37,6 @@ def read_temp_raw():
 	lines = f.readlines()
 	f.close()
 	return lines
- 
-def read_temp():
-	lines = read_temp_raw()
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
-		lines = read_temp_raw()
-	equals_pos = lines[1].find('t=')
-	if equals_pos != -1:
-		temp_string = lines[1][equals_pos+2:]
-		temp_c = float(temp_string) / 1000.0
-		temp_f = temp_c * 9.0 / 5.0 + 32.0
 
 # Return true if host says snoozed, false otherwise
 def check_snooze():
@@ -60,6 +49,17 @@ def check_snooze():
 	except: 
 		print("could not check snooze service")
 	return False
+ 
+def read_temp():
+	lines = read_temp_raw()
+	while lines[0].strip()[-3:] != 'YES':
+		time.sleep(0.2)
+		lines = read_temp_raw()
+	equals_pos = lines[1].find('t=')
+	if equals_pos != -1:
+		temp_string = lines[1][equals_pos+2:]
+		temp_c = float(temp_string) / 1000.0
+		temp_f = temp_c * 9.0 / 5.0 + 32.0
 
 	# TEST OVERRIDE
 	# temp_c = 30.0
@@ -110,7 +110,6 @@ def check_snooze():
 		print("temp within safe zone")
 
 	return temp_c, temp_f
-
 	
 while True:
 	print(read_temp())	
